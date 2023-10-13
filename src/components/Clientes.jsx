a8215f4e645eb976081c24858c65eb200feddfbb
mcamaya <camiamayar@gmail.com>
2023-10-13 06:58:57 -0500

    Tabla dinámica

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Table from "./Table";
 
 export default function Clientes() {
+  const urlApi = "http://localhost:8080/api/v1/clientes";
+  const dataKeys = [
+    "nombre",
+    "numeroDocumento",
+    "tipoDocumento",
+    "celular",
+    "email"
+  ]
+
   return (
     <div>
       <h1>Clientes</h1>
+      <Table 
+        urlApi={urlApi}
+        dataKeys={dataKeys}
+      />
     </div>
-  )
+  );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function Table() {
+export default function Table({ urlApi, dataKeys }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(urlApi)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => alert(err));
+  });
+
   return (
     <div>
       <table>
         <thead>
-          <th>ID</th>
-          <th>Nombre</th>
-          <th># Doc</th>
-          <th>tipo Doc</th>
-          <th>Celular</th>
-          <th>Email</th>
+          <tr>
+            {dataKeys.map((e, index) => (
+              <th key={index}>{e}</th>
+            ))}
+          </tr>
         </thead>
+        <tbody>
+          {
+            data.map((eData, index) => (
+              <tr key={index}>
+                {
+                  dataKeys.map((eKey, index) => (
+                    <td key={index}>{eData[eKey]}</td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </div>
-  )
+  );
 }
